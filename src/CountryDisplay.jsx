47a622ec238733fbfbb393c457ca6f9c387f70b1
Michael Ngo <michaelngo1337@yahoo.com>
2023-11-03 10:52:06 -0700

    exercise 2.19

--- src/CountryDisplay.jsx
+++ src/CountryDisplay.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
+
 const CountryDisplay = ({ country }) => {
+
+  const [press, setPress] = useState(false);
+  
   return (
     <>
-      <h2>{country.name.common}</h2>
-      <p>capital {country.capital[0]}</p>
-      <p>area {country.area}</p>
-      <h3>languages:</h3>
-      <ul>
-        {Object.entries(country.languages).map(([key, value]) => <li>{value}</li>)}
-      </ul>
-      <img src={country.flags.png} alt={country.flags.alt} />
+      {press ?
+        <>
+          <h2>{country.name.common}</h2>
+          <p>capital {country.capital[0]}</p>
+          <p>area {country.area}</p>
+          <h3>languages:</h3>
+          <ul>
+            {Object.entries(country.languages).map(([key, value]) => <li>{value}</li>)}
+          </ul>
+          <img src={country.flags.png} alt={country.flags.alt} />
+        </>
+        : <>
+            <span>{country.name.common}</span>
+            <button onClick={() => setPress(!press)}>show</button>
+            <br />
+          </>}
     </>
   )
 }
--- src/FilterCountries.jsx
+++ src/FilterCountries.jsx
@@ -1,23 +1,10 @@
-import { useState } from "react"
 import CountryDisplay from "./CountryDisplay"
 
 const FilterCountries = ({ countries }) => {
-
-  const [press, setPress] = useState(false);
-
-  const handlePress = () => {
-    console.log("button pressed moffo")
-    setPress(!press);
-  }
-
   return (
     (countries.length > 10) ? <p>too many results, please narrow search</p>
     : (countries.length > 1) ? countries.map(country => (
-      <>
-        <span>{country.name.common}</span>
-        <button onClick={handlePress}>show</button>
-        <br />
-      </>
+      <CountryDisplay country={country} />
     ))
       : (countries.length) ? <CountryDisplay country={countries[0]} />
         : <p>no matches</p>
